063244016268ba69ee33b860682c4465df74f3e4
AndrxwWxng <andrewwang123118@gmail.com>
2024-08-28 22:14:13 -0500

    save

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+
+const aboutPage = () => {
+    return (
+        <div className="py-2 px-4 z-50 items-center justify-between">
+        <span>The Turing Club is all about spreading computer science. It has been a key part of the CS Forum at Allen High School for over half a decade. </span>
+        </div>
+    );
+};
+
+export default aboutPage;
