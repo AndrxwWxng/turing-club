fb89f62c3a0ba7b98724fa323ad631d304cee018
codingforyu <158034944+codingforyu@users.noreply.github.com>
2024-08-29 20:56:52 -0500

    upcoming even in home pg and basis for signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ import Head from 'next/head';
 import Navbar from '@/components/Navbar';
 import { motion } from 'framer-motion';
 import Hero from '@/components/Hero';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const HomePage = () => {
   return (
@@ -17,6 +23,26 @@ const HomePage = () => {
         {/* <Navbar /> */}
         <Hero />
       </div>
+      <center>
+      <div className="bg-black py-3 md:flex-row justify-between gap-8">
+          <Card className="relative max-w-xl flex-1 p-6 shadow-lg rounded-lg bg-indigo-600 text-white width-50">
+            <div className="bg-indigo-900 rounded-lg">
+            <CardHeader>
+              <CardTitle className="text-center">Upcoming Event</CardTitle>
+            </CardHeader>
+            </div>
+            <div className="pt-4">
+            <CardContent>
+              <p className="text-lg">
+                Location: n/a <br></br>
+                Topic: n/a <br></br>
+                Speaker: n/a <br></br>
+              </p>
+            </CardContent>
+            </div>
+          </Card>
+      </div>
+      </center>
     </>
   );
 };
