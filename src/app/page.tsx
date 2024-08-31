2672f4b7ea05a271fa83e3c65e81838cf9e18479
AndrxwWxng <andrewwang123118@gmail.com>
2024-08-30 23:24:09 -0500

    optimized and formatted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,9 @@
 import React from 'react';
 import Head from 'next/head';
 import Navbar from '@/components/Navbar';
-import { motion } from 'framer-motion';
 import Hero from '@/components/Hero';
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import UpcomingPresentations from '@/components/UpcomingPresentations';
+import  Footer from '@/components/Footer';
 
 const HomePage = () => {
   return (
@@ -20,95 +15,10 @@ const HomePage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="min-h-screen bg-indigo-900">
-        {/* <Navbar /> */}
         <Hero />
       </div>
-      <center>
-      <div className="bg-white py-3 md:flex-row justify-between gap-8 width-75">
-          <Card className="relative max-w-xl max-h-m flex-1 p-6 shadow-lg rounded-lg bg-indigo-900 text-white height-10 width-50">
-            <div className="rounded-lg">
-            <CardHeader>
-              <CardTitle className="text-center">Upcoming Presentations</CardTitle>
-            </CardHeader>
-            </div>
-          </Card>
-      </div>
-      </center>
-      <div className="flex container mx-auto px-4 py-8">
-      <div className="bg-white py-3 px-5 md:flex-row justify-between gap-8 width-75">
-          <Card className="relative max-w-xl flex-1 p-6 shadow-lg rounded-lg bg-indigo-600 text-white width-50">
-            <div className="bg-indigo-900 rounded-lg">
-            <CardHeader>
-              <CardTitle className="text-center">Presentation Name</CardTitle>
-            </CardHeader>
-            </div>
-            <div className="pt-4">
-            <CardContent>
-              <p className="text-lg">
-                Location: n/a <br></br>
-                Topic: n/a <br></br>
-                Speaker: n/a <br></br>
-              </p>
-            </CardContent>
-            </div>
-          </Card>
-      </div>
-      <div className="bg-white py-3 px-5 md:flex-row justify-between gap-8 width-75">
-          <Card className="relative max-w-xl flex-1 p-6 shadow-lg rounded-lg bg-indigo-600 text-white width-50">
-            <div className="bg-indigo-900 rounded-lg">
-            <CardHeader>
-              <CardTitle className="text-center">Presentation Name</CardTitle>
-            </CardHeader>
-            </div>
-            <div className="pt-4">
-            <CardContent>
-              <p className="text-lg">
-                Location: n/a <br></br>
-                Topic: n/a <br></br>
-                Speaker: n/a <br></br>
-              </p>
-            </CardContent>
-            </div>
-          </Card>
-      </div>
-      <div className="bg-white py-3 px-5 md:flex-row justify-between gap-8 width-75">
-          <Card className="relative max-w-xl flex-1 p-6 shadow-lg rounded-lg bg-indigo-600 text-white width-50">
-            <div className="bg-indigo-900 rounded-lg">
-            <CardHeader>
-              <CardTitle className="text-center">Presentation Name</CardTitle>
-            </CardHeader>
-            </div>
-            <div className="pt-4">
-            <CardContent>
-              <p className="text-lg">
-                Location: n/a <br></br>
-                Topic: n/a <br></br>
-                Speaker: n/a <br></br>
-              </p>
-            </CardContent>
-            </div>
-          </Card>
-      </div>
-      <div className="bg-white py-3 px-5 md:flex-row justify-between gap-8 width-75">
-          <Card className="relative max-w-xl flex-1 p-6 shadow-lg rounded-lg bg-indigo-600 text-white width-50">
-            <div className="bg-indigo-900 rounded-lg">
-            <CardHeader>
-              <CardTitle className="text-center">Presentation Name</CardTitle>
-            </CardHeader>
-            </div>
-            <div className="pt-4">
-            <CardContent>
-              <p className="text-lg">
-                Location: n/a <br></br>
-                Topic: n/a <br></br>
-                Speaker: n/a <br></br>
-              </p>
-            </CardContent>
-            </div>
-          </Card>
-      </div>
-      </div>
-
+      <UpcomingPresentations />
+      <Footer />
     </>
   );
 };
